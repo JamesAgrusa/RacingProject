2d9c9819d88bb3def7034b180aaafccdd4b4b5c1
JamesAgrusa <137592319+JamesAgrusa@users.noreply.github.com>
2025-01-07 19:58:39 -0500

    Project Creation
    Starting Project

---
+++ Source/RacingProject/RacingProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RacingProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RacingProject, "RacingProject" );
